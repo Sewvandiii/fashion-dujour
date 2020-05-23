90d928fcfc8071e37d405e59f443956a4c59c3b9
Sewvandiii <sewvandipw.98@gmail.com>
2020-05-24 01:43:04 +0530

    Update Frontend

--- frontend/src/components/signup/SignUpForm.jsx
+++ frontend/src/components/signup/SignUpForm.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './signupStyles.css'
-import swal from 'sweetalert';
+import swal from 'sweetalert'
+import emailjs from 'emailjs-com';
 
 const initialState = {
   managerName: '',
@@ -16,8 +17,7 @@ const initialState = {
 }
 
 class SignUpForm extends Component {
-
-  state = initialState;
+  state = initialState
 
   inputChangeHandler = e => {
     const { name, value } = e.target
@@ -27,78 +27,100 @@ class SignUpForm extends Component {
   }
 
   validate = () => {
-    let mnameerror = "";
-    let cnameerror = "";
-    let memailerror = "";
-    let passworderror = "";
-    let conpassworderror = "";
+    let mnameerror = ''
+    let cnameerror = ''
+    let memailerror = ''
+    let passworderror = ''
+    let conpassworderror = ''
 
     if (!this.state.managerName) {
-      mnameerror = "Enter Store Manager Name";
+      mnameerror = 'Enter Store Manager Name'
     }
 
     if (!this.state.companyName) {
-      cnameerror = "Enter Company Name";
+      cnameerror = 'Enter Company Name'
     }
 
-    if (!this.state.managerEmail.includes("@")) {
-      memailerror = "Invalid Email";
+    if (!this.state.managerEmail.includes('@')) {
+      memailerror = 'Invalid Email'
     }
 
     if (!this.state.password) {
-      passworderror = "Enter Password";
+      passworderror = 'Enter Password'
     }
 
     if (!this.state.conPassword) {
-      conpassworderror = "Confirm Password";
+      conpassworderror = 'Confirm Password'
     }
 
-    if (mnameerror || cnameerror || memailerror || passworderror || conpassworderror) {
-      this.setState({ mnameerror, cnameerror, memailerror, passworderror, conpassworderror });
-      return false;
+    if (
+      mnameerror ||
+      cnameerror ||
+      memailerror ||
+      passworderror ||
+      conpassworderror
+    ) {
+      this.setState({
+        mnameerror,
+        cnameerror,
+        memailerror,
+        passworderror,
+        conpassworderror
+      })
+      return false
     }
-    swal("Store Manager Added Successfully!", "No warnings! ", "success");
-    return true;
-  };
+    swal('Store Manager Added Successfully!', 'No warnings! ', 'success')
+    return true
+  }
 
-  formSubmitHandler = (e) => {
-    const isValid = this.validate();
+  formSubmitHandler = e => {
+    const isValid = this.validate()
     if (isValid) {
-    console.log(this.state.managerName);
-    console.log(this.state.companyName);
-    console.log(this.state.managerEmail);
-    console.log(this.state.password);
-    console.log(this.state.conPassword);
+      console.log(this.state.managerName)
+      console.log(this.state.companyName)
+      console.log(this.state.managerEmail)
+      console.log(this.state.password)
+      console.log(this.state.conPassword)
       //clear form
-      this.setState(initialState);
+      this.setState(initialState)
     }
 
-    if (this.state.managerName == null && this.state.companyName == null && this.state.managerEmail == null && this.state.password == null && this.state.conPassword == null) {
-      return alert("Cannot submit empty fields")
+    if (
+      this.state.managerName == null &&
+      this.state.companyName == null &&
+      this.state.managerEmail == null &&
+      this.state.password == null &&
+      this.state.conPassword == null
+    ) {
+      return alert('Cannot submit empty fields')
     }
     if (this.state.password !== this.state.conPassword) {
-      alert("Password Mismatch!")
+      alert('Password Mismatch!')
       return
     }
 
+    let thisState = this;
+    let stateaccess = this.state;
     alert(JSON.stringify(this.state))
     fetch('http://localhost:5000/manager', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        Accept: 'application/json'
       },
       body: JSON.stringify({
-        'managerName': this.state.managerName,
-        'companyName': this.state.companyName,
-        'managerEmail': this.state.managerEmail,
-        'password': this.state.password,
-        'conPassword': this.state.conPassword,
+        managerName: this.state.managerName,
+        companyName: this.state.companyName,
+        managerEmail: this.state.managerEmail,
+        password: this.state.password,
+        conPassword: this.state.conPassword
       })
     })
       .then(function (callback) {
         console.log(callback.json())
         // alert("Submitted Successfully!");
+        //console.log(thisState.state.managerEmail)
+        thisState.sendEmail(stateaccess.managerEmail, stateaccess.conPassword);
       })
       .catch(error => {
         console.log(error)
@@ -113,17 +135,34 @@ class SignUpForm extends Component {
     })
   }
 
-  render() {
+  sendEmail = (userEmail, userPW) => {
+    console.log(userEmail)
+    fetch('http://localhost:5000/email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: userEmail,
+        pw: userPW
+      })
+    }).then(function(callback){
+      console.log("Email Sent")
+    }).catch(error => {
+      console.log(error)
+    })
+  }
 
+  render () {
     return (
-
       <div>
         <div className='container'>
           <form onSubmit={this.formSubmitHandler}>
-
             <div className='form-group'>
-
-              <label><i class="fa fa-user" aria-hidden="true"></i>&nbsp;Store Manager Name</label>
+              <label>
+                <i class='fa fa-user' aria-hidden='true'></i>&nbsp;Store Manager
+                Name
+              </label>
               <input
                 type='text'
                 onChange={this.inputChangeHandler}
@@ -132,13 +171,16 @@ class SignUpForm extends Component {
                 value={this.state.managerName}
                 required
               />
-              <div style={{ fontSize: 12, color: "green" }}>
+              <div style={{ fontSize: 12, color: 'green' }}>
                 {this.state.mnameerror}
               </div>
             </div>
 
             <div className='form-group'>
-              <label htmlFor='exampleInputEmail1'><i class="fa fa-building" aria-hidden="true"></i>&nbsp;Company Name</label>
+              <label htmlFor='exampleInputEmail1'>
+                <i class='fa fa-building' aria-hidden='true'></i>&nbsp;Company
+                Name
+              </label>
               <input
                 type='text'
                 onChange={this.inputChangeHandler}
@@ -148,12 +190,15 @@ class SignUpForm extends Component {
                 aria-describedby='emailHelp'
                 required
               />
-              <div style={{ fontSize: 12, color: "green" }}>
+              <div style={{ fontSize: 12, color: 'green' }}>
                 {this.state.cnameerror}
               </div>
             </div>
             <div className='form-group'>
-              <label htmlFor='exampleInputEmail1'><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;Store Manager Email</label>
+              <label htmlFor='exampleInputEmail1'>
+                <i class='fa fa-envelope' aria-hidden='true'></i>&nbsp;Store
+                Manager Email
+              </label>
               <input
                 name='managerEmail'
                 type='email'
@@ -162,10 +207,12 @@ class SignUpForm extends Component {
                 value={this.state.managerEmail}
                 required
               />
-
             </div>
             <div className='form-group'>
-              <label htmlFor='exampleInputPassword1'><i class="fa fa-unlock-alt" aria-hidden="true"></i>&nbsp;Password</label>
+              <label htmlFor='exampleInputPassword1'>
+                <i class='fa fa-unlock-alt' aria-hidden='true'></i>
+                &nbsp;Password
+              </label>
               <input
                 type='password'
                 name='password'
@@ -175,12 +222,15 @@ class SignUpForm extends Component {
                 value={this.state.password}
                 required
               />
-              <div style={{ fontSize: 12, color: "green" }}>
+              <div style={{ fontSize: 12, color: 'green' }}>
                 {this.state.passworderror}
               </div>
             </div>
             <div className='form-group'>
-              <label htmlFor='exampleInputPassword1'><i class="fa fa-unlock-alt" aria-hidden="true"></i>&nbsp;Confirm Password</label>
+              <label htmlFor='exampleInputPassword1'>
+                <i class='fa fa-unlock-alt' aria-hidden='true'></i>&nbsp;Confirm
+                Password
+              </label>
               <input
                 name='conPassword'
                 type='password'
@@ -190,20 +240,18 @@ class SignUpForm extends Component {
                 value={this.state.conPassword}
                 required
               />
-              <div style={{ fontSize: 12, color: "green" }}>
+              <div style={{ fontSize: 12, color: 'green' }}>
                 {this.state.conpassworderror}
               </div>
             </div>
             <br></br>
-          
+
             <button type='submit' className='btn-submit'>
               Add Store Manager
-          </button>
-         
+            </button>
           </form>
         </div>
       </div>
-
     )
   }
 }
